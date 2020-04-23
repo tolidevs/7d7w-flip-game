945cf34f0ff31c60a98620b90318173623d6192a
flatironschool <t.benson@live.co.uk>
2020-04-23 11:37:46 +0100

    added jazzy jeff picture to winner modal as can't really see card

--- src/Winner.js
+++ src/Winner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Jazzy from './images/jazzy-jeff.jpg'
 
 const Winner = ({ handleClose, show, count }) => {
     const tries =  count === 1 ? 'one try' : `${count} tries`
@@ -7,6 +8,7 @@ const Winner = ({ handleClose, show, count }) => {
         <div className={show ? "modal display-block" : "modal display-none"} onClick={handleClose}>
             <section className="modal-main">
                 <h1>WINNER!!!</h1>
+                <img src={Jazzy} alt="DJ Jazzy Jeff"></img>
                 <h2>You found Jazzy Jeff after {tries}!</h2>
             </section>
         </div>
